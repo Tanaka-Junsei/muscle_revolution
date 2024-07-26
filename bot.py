ca2386a99372275fe143e090f29811bf0a715f74
Tanaka-Junsei <junsei.tanaka.2103@gmail.com>
2024-07-27 02:29:52 +0900

    ログ追加

--- bot.py
+++ bot.py
@@ -1,14 +1,22 @@
-# bot.py
-
 from dotenv import load_dotenv
 import os
 import discord
+import logging
+
+# ログ設定
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s:%(message)s')
 
 # .env ファイルを読み込む
 load_dotenv()
 
 # 自分のBotのアクセストークンを取得
 TOKEN = os.getenv('DISCORD_BOT_TOKEN')
+
+# トークンが正しく読み込まれているかを確認（デバッグ用）
+if TOKEN is None:
+    logging.error("DISCORD_BOT_TOKEN is not set")
+else:
+    logging.info(f"Token: {TOKEN}")
 
 # 必要なintentsを設定
 intents = discord.Intents.default()
@@ -20,18 +28,19 @@ client = discord.Client(intents=intents)
 # 起動時に動作する処理
 @client.event
 async def on_ready():
-    # 起動したらターミナルにログイン通知が表示される
-    print('ログインしました')
+    logging.info('ログインしました')
 
 # メッセージ受信時に動作する処理
 @client.event
 async def on_message(message):
     # メッセージ送信者がBotだった場合は無視する
     if message.author.bot:
+        logging.info('Botからのメッセージは無視します')
         return
     # 「/neko」と発言したら「にゃーん」が返る処理
     if message.content == '/neko':
         await message.channel.send('ちくび')
+        logging.info('ちくび と返しました')
 
 # Botの起動とDiscordサーバーへの接続
 client.run(TOKEN)
