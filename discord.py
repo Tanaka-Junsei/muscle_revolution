b1edea2a78891f82ff047a794b4d8195813bcc4f
Tanaka-Junsei <junsei.tanaka.2103@gmail.com>
2024-07-27 01:43:08 +0900

    first commit

---
+++ discord.py
@@ -0,0 +1,32 @@
+# インストールした dotenv を読み込む
+from dotenv import load_dotenv
+import os
+import discord
+
+# .env ファイルを読み込む
+load_dotenv()
+
+# 自分のBotのアクセストークンを取得
+TOKEN = os.getenv('DISCORD_BOT_TOKEN')
+
+# 接続に必要なオブジェクトを生成
+client = discord.Client()
+
+# 起動時に動作する処理
+@client.event
+async def on_ready():
+    # 起動したらターミナルにログイン通知が表示される
+    print('ログインしました')
+
+# メッセージ受信時に動作する処理
+@client.event
+async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    # 「/neko」と発言したら「にゃーん」が返る処理
+    if message.content == '/neko':
+        await message.channel.send('にゃーん')
+
+# Botの起動とDiscordサーバーへの接続
+client.run(TOKEN)
