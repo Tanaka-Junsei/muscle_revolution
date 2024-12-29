488188c530ee7446ff69716150cbbedb72bcd123
Tanaka-Junsei <ca02447@cartahd.com>
2024-12-30 00:18:13 +0900

    early returnとコメント整理

--- bot.py
+++ bot.py
@@ -7,7 +7,6 @@ import logging
 from dotenv import load_dotenv
 import pytz
 
-# ログ設定
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s:%(message)s')
 
 load_dotenv()
@@ -21,26 +20,22 @@ intents = discord.Intents.default()
 intents.message_content = True
 intents.messages = True
 
-# 接続に必要なオブジェクトを生成
 client = discord.Client(intents=intents)
 
 NEXT_POST_TIME = None
-JST = pytz.timezone('Asia/Tokyo')  # 日本標準時
+JST = pytz.timezone('Asia/Tokyo')
 
 # 起動時チェック
 @client.event
 async def on_ready():
     logging.info('logged in as {0.user}'.format(client))
-    # ループで次の投稿時間をチェック
     client.loop.create_task(check_next_post_time())
 
-# NEXT_POST_TIMEになった時に、指定のチャンネルにメッセージを送信
 async def check_next_post_time():
     global NEXT_POST_TIME
     await client.wait_until_ready()
     channel = client.get_channel(CHANNEL_ID)
     while not client.is_closed():
-        # 次の投稿時間になったらメッセージを送信
         if NEXT_POST_TIME and datetime.datetime.now(JST) >= NEXT_POST_TIME:
             user = await client.fetch_user(TARGET_USER_ID)
             await channel.send(f'{user.mention} KAZUO is WATCHING YOU')
@@ -51,26 +46,26 @@ async def check_next_post_time():
 @client.event
 async def on_message(message):
     global NEXT_POST_TIME
-    if message.channel.id != CHANNEL_ID:
+
+    if message.channel.id != CHANNEL_ID or message.author.bot:
         return
-    if message.author.bot:
-        return
+
     if message.content == '/muscle':
         await message.reply('痩せろデブ')
         return
-    # 画像を送信してから次の投稿までの時間を返信
-    if message.author.id == TARGET_USER_ID:
-        if message.attachments:
-            for attachment in message.attachments:
-                if attachment.content_type.startswith('image/'):
-                    # 次の画像の投稿時間を設定
-                    NEXT_POST_TIME = datetime.datetime.now(JST) + datetime.timedelta(days=1, hours=2)
-                    await message.reply(f'次は {NEXT_POST_TIME.strftime("%Y-%m-%d %H:%M:%S")} まで')
-                    return
-    else:
-        # 10%の確率でメッセージを送信
+
+    if message.author.id != TARGET_USER_ID:
         if random.random() < 0.1:
             await message.reply('お前もマッチョにならないか？')
-            return
+        return
+
+    if not message.attachments:
+        return
+
+    for attachment in message.attachments:
+        if attachment.content_type.startswith('image/'):
+            NEXT_POST_TIME = datetime.datetime.now(JST) + datetime.timedelta(days=1, hours=2)
+            await message.reply(f'次は {NEXT_POST_TIME.strftime("%Y-%m-%d %H:%M:%S")} まで')
+            return 
 
 client.run(TOKEN)
